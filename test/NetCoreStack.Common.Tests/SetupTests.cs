b5eccf4d1d7284a860e0badbb54fb599a2b2b091
Gencebay Demir <gencebay.demir@gmail.com>
2016-12-13 19:12:51 +0300

    Test framework

---
+++ test/NetCoreStack.Common.Tests/SetupTests.cs
@@ -0,0 +1,18 @@
+﻿using NetCoreStack.Common.Tests.Types;
+using System.Reflection;
+using Xunit;
+
+namespace NetCoreStack.Common.Tests
+{
+    public class SetupTests
+    {
+        [Fact]
+        public void Setup_NetCoreStack()
+        {
+            var controller = typeof(LookupController);
+
+            // Assert
+            Assert.True(typeof(IApiContract).IsAssignableFrom(controller));
+        }
+    }
+}
---
+++ test/NetCoreStack.Common.Tests/Types/LookupController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreStack.Common.Tests.Types
+{
+    public class LookupController : ControllerBase, IApiContract
+    {
+        public LookupController()
+        {
+
+        }
+    }
+}
