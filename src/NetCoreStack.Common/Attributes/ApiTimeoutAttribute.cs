9c19ac3608a39cac2c46e037f708b6b026acffb9
Gencebay Demir <gencebay.demir@gmail.com>
2016-12-25 20:01:51 +0300

    ApiTimeoutAttribute added

---
+++ src/NetCoreStack.Common/Attributes/ApiTimeoutAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NetCoreStack.Common
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public sealed class ApiTimeoutAttribute : Attribute
+    {
+        public TimeSpan Timeout { get; private set; }
+
+        /// <summary>
+        /// Timeout seconds
+        /// </summary>
+        /// <param name="seconds"></param>
+        public ApiTimeoutAttribute(int seconds)
+        {
+            Timeout = TimeSpan.FromSeconds(seconds);
+        }
+    }
+}
